aa92e6ce70d2e434d66375bb499aeab4ecb38b50
Renato Fringuello <65839026+RenatoFringuello@users.noreply.github.com>
2023-01-07 19:45:02 +0100

    add docs folder

---
+++ docs/js/script.js
@@ -0,0 +1,60 @@
+const { createApp } = Vue;
+const DateTime = luxon.DateTime;
+
+createApp({
+    data(){
+        return{
+            timer : '',
+            timeRaw : '01:00',
+            timeFullSeconds : 0,
+            timeCurrentSeconds : 0,
+            timerToggle : false,
+            buttonState : 'Rest',
+            activeSlide : 0,
+            workoutsPlan : workouts
+        }
+    },
+    methods : {
+        getMappedHeight(maxA, a, maxB){
+            return (a * maxB) / maxA;
+        },
+        startTimer(){
+            if(this.timerToggle){
+                //on click when open
+
+                //change button state name
+                this.buttonState = 'Rest';
+                //stop counting
+                clearInterval(timer);
+            }
+            else{
+                //on click when closed
+                
+                //change button state name
+                this.buttonState = 'Stop';
+                //get target time
+                const timeTarget = DateTime.now().plus({minutes:this.timeRaw.split(':')[0], seconds:this.timeRaw.split(':')[1]});
+                //get now
+                let timeCurrent = DateTime.now();
+                //get max time
+                this.timeFullSeconds = ((timeTarget.c.minute - timeCurrent.c.minute) * 60) + timeTarget.c.second - timeCurrent.c.second;
+                
+                //start counting
+                timer = setInterval(()=>{
+                    //get now
+                    timeCurrent = DateTime.now();
+                    //subtract every seconds delayed
+                    this.timeCurrentSeconds = ((timeTarget.c.minute - timeCurrent.c.minute) * 60) + timeTarget.c.second - timeCurrent.c.second;
+                    console.log(this.timeCurrentSeconds);
+                    if(this.timeCurrentSeconds <= 0){
+                        //stop timer
+                        this.timerToggle = false;
+                        clearInterval(timer);
+                    }
+                }, 100);
+            }
+            //toggle it
+            this.timerToggle = !this.timerToggle;
+        }
+    }
+}).mount('#app');
---
+++ docs/js/workouts.js
@@ -0,0 +1,54 @@
+const workouts = [
+    {
+        icon: "💣",
+        group: "shoulder", 
+        exercises : [
+            {exerciseName : "alzate laterali da seduto", sets:4, reps:10},
+            {exerciseName : "alzate frontali da seduto", sets:4, reps:10},
+            {exerciseName : "alzate laterali in stripping al multi flight", sets:3, reps: "10+10+10 a scalare"},
+            {exerciseName : "deltoid press", sets:5, reps:10},
+            {exerciseName : "spinte manubri pesanti", sets:4, reps:10}
+        ]
+    },
+    {
+        icon: "💪", group: "bi & tri", exercises : [
+            {exerciseName : "curl machine in stripping", sets:3, reps: "10+10+10 a scalare"},
+            {exerciseName : "curl bilanciere", sets:4, reps:10},
+            {exerciseName : "curl martello", sets:3, reps:10},
+            {exerciseName : "curl concentrato manubrio singolo", sets:4, reps:10},
+            {exerciseName : "ercolina con corda in stripping", sets:3, reps: "10+10+10 a scalare"},
+            {exerciseName : "french press", sets:4, reps:10},
+            {exerciseName : "triceps machine", sets:3, reps:10},
+            {exerciseName : "french press concentrato manubrio singolo", sets:3, reps:10}
+        ]
+    },
+    {
+        icon: "🧰", group: "chest", exercises : [
+            {exerciseName : "panca piana", sets:8, reps:8},
+            {exerciseName : "spinte manubri", sets:3, reps:12},
+            {exerciseName : "panca inclinata al multipower", sets:3, reps:10},
+            {exerciseName : "croci ai cavi alti + ai cavi bassi", sets:4, reps: "10+10"},
+            {exerciseName : "croci manubri su panca inclinata", sets:3, reps:10}
+        ]
+    },
+    {
+        icon: "🦵", group: "leg", exercises : [
+            {exerciseName : "leg press 45° tecnica rest pause", sets:4, reps: "5(10\"rest)+5(10\"rest)+5"},
+            {exerciseName : "leg ext in stripping", sets:3, reps: "10+10+10+10"},
+            {exerciseName : "front squat al multipower", sets:4, reps:10},
+            {exerciseName : "affondi inversi manubri", sets:3, reps:10},
+            {exerciseName : "hack squat", sets:3, reps:10},
+            {exerciseName : "vertical leg press", sets:3, reps:10},
+            {exerciseName : "calf in piedi", sets:5, reps:20}
+        ]
+    },
+    {
+        icon: "🔙", group: "back", exercises : [
+            {exerciseName : "stacchi da terra", sets:4, reps:10},
+            {exerciseName : "rematore bilanciere presa prona", sets:3, reps:12},
+            {exerciseName : "low row", sets:3, reps:10},
+            {exerciseName : "dorsy bar + dorsy bar inclinata", sets:3, reps: "10+10"},
+            {exerciseName : "lat larga avanti", sets:4, reps:8}
+        ]
+    }
+]
